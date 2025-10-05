6dcc752b5ca98e1cb0e886c91791b23dc4e9ac2a
GOKUL SIVA123 <gokulsiva1101@gmail.com>
2025-10-05 18:02:28 +0530

    dist updated

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Add basename for GitHub Pages subpath */}
+      <BrowserRouter basename="/Gokulsportfolio104">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
